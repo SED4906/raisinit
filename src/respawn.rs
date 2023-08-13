d98cb70a5c1c4f550ff85fb660ded629656533fc
SED4906 <sed4906birdie@gmail.com>
2023-08-13 16:09:22 -0700

    working respawn program

--- src/respawn.rs
+++ src/respawn.rs
@@ -1,19 +1,30 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{panic::PanicInfo, arch::asm};
 use raisinlib::{println,exec::spawn};
 use syscalls::{syscall, Sysno};
 
 #[no_mangle]
-pub extern "C" fn _start(argc: i32, argv: *const *const u8) -> i32 {
+pub extern "C" fn _start() {
+    unsafe{asm!(
+        "pop rbp;", // C compiler will push rbp
+        "pop rdi;",        // argc
+        "mov rsi,rsp;", // argv
+        "and rsp, -16;",
+        "call main;",
+    )};
+}
+
+#[no_mangle]
+pub extern "C" fn main(argc: i32, argv: *const *const u8) -> i32 {
     if argc > 1 {
         loop {
             if let Ok(pid) = unsafe{syscall!(Sysno::fork)} {
                 if pid == 0 {
                     unsafe{syscall!(Sysno::execve,*argv.add(1),argv.add(2),0)};
                 } else {
-                    unsafe{syscall!(Sysno::waitid,1,pid,0,0)};
+                    unsafe{syscall!(Sysno::wait4,pid,0,0,0)};
                 }
             }
         }
