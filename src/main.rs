fb1451a46297993b5f1f108b333a1ab2b2df78fe
SED4906 <sed4906birdie@gmail.com>
2023-08-13 19:23:39 -0700

    run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
-use raisinlib::{println, io::{File,Read},exec::spawn};
+use raisinlib::{
+    exec::spawn,
+    io::{File, Read},
+    println,
+};
 use syscalls::{syscall, Sysno};
-
 
 const MAX_FILE_SIZE: usize = 2048;
 
 #[no_mangle]
 pub extern "C" fn _start() {
     println!("raisINIT");
-    let mut buf: [u8; MAX_FILE_SIZE+1] = [0; MAX_FILE_SIZE+1];
-    let mut args: [*const u8; MAX_FILE_SIZE+1] = [core::ptr::null(); MAX_FILE_SIZE+1];
+    let mut buf: [u8; MAX_FILE_SIZE + 1] = [0; MAX_FILE_SIZE + 1];
+    let mut args: [*const u8; MAX_FILE_SIZE + 1] = [core::ptr::null(); MAX_FILE_SIZE + 1];
     let mut file = File::open("/etc/raisinrc\0").unwrap();
     let actual_size = file.read(&mut buf).unwrap();
     file.close();
-    let mut buf_line: [u8; MAX_FILE_SIZE+1] = [0; MAX_FILE_SIZE+1];
+    let mut buf_line: [u8; MAX_FILE_SIZE + 1] = [0; MAX_FILE_SIZE + 1];
     #[allow(unused_assignments)]
     let mut line_start = 0;
     let mut line_end = 0;
     for i in 0..actual_size {
         match buf[i] {
-            0|b'\r'|b'\n' => {
+            0 | b'\r' | b'\n' => {
                 line_start = line_end;
                 line_end = i;
-                if line_end-line_start <= 1 {
-                    line_end = i+1;
+                if line_end - line_start <= 1 {
+                    line_end = i + 1;
                     continue;
                 }
                 buf_line.fill(0);
@@ -33,25 +36,29 @@ pub extern "C" fn _start() {
                 buf_line[..(line_end - line_start)].copy_from_slice(&buf[line_start..line_end]);
                 split_into_cmdline(&mut buf_line, &mut args);
                 let _ = spawn(&buf_line, &args);
-            },
+            }
             _ => {}
         }
     }
-    loop{unsafe{let _ = syscall!(Sysno::pause);}}
+    loop {
+        unsafe {
+            let _ = syscall!(Sysno::pause);
+        }
+    }
 }
 
 #[panic_handler]
 fn rust_panic(_: &PanicInfo) -> ! {
     println!("init: panic!!!");
-    loop{}
+    loop {}
 }
 
 fn split_into_cmdline(buf: &mut [u8], args: &mut [*const u8]) {
     let mut i = 0;
-    for arg in buf.split(|c| c==&b' ') {
+    for arg in buf.split(|c| c == &b' ') {
         i += 1;
         if i > 0 {
-            args[i-1] = arg.as_ptr();
+            args[i - 1] = arg.as_ptr();
         }
     }
     for c in buf {
--- src/respawn.rs
+++ src/respawn.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::{panic::PanicInfo, arch::asm};
+use core::{arch::asm, panic::PanicInfo};
 use raisinlib::println;
 use syscalls::{syscall, Sysno};
 
@@ -9,8 +9,8 @@ use syscalls::{syscall, Sysno};
 #[no_mangle]
 pub unsafe extern "C" fn _start() {
     asm!(
-        "pop rbp;", // C compiler will push rbp
-        "pop rdi;",        // argc
+        "pop rbp;",     // C compiler will push rbp
+        "pop rdi;",     // argc
         "mov rsi,rsp;", // argv
         "and rsp, -16;",
         "call main;",
@@ -24,9 +24,9 @@ pub unsafe extern "C" fn main(argc: i32, argv: *const *const u8) -> i32 {
         loop {
             if let Ok(pid) = syscall!(Sysno::fork) {
                 if pid == 0 {
-                    let _ = syscall!(Sysno::execve,*argv.add(1),argv.add(2),0);
+                    let _ = syscall!(Sysno::execve, *argv.add(1), argv.add(2), 0);
                 } else {
-                    let _ = syscall!(Sysno::wait4,pid,0,0,0);
+                    let _ = syscall!(Sysno::wait4, pid, 0, 0, 0);
                 }
             }
         }
@@ -37,5 +37,5 @@ pub unsafe extern "C" fn main(argc: i32, argv: *const *const u8) -> i32 {
 #[panic_handler]
 fn rust_panic(_: &PanicInfo) -> ! {
     println!("respawn: panic!!!");
-    loop{}
+    loop {}
 }
